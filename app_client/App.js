c5714ca5c13393906b26b72a71e0f400f63f0d39
simaonogueira101 <simao.nogueira66@gmail.com>
2017-06-03 12:01:43 +0100

    Adds location to maps and fixes other things.

--- app_client/App.js
+++ app_client/App.js
@@ -13,6 +13,7 @@ import {
 import { StackNavigator } from 'react-navigation';
 import LinearGradient from 'react-native-linear-gradient';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import axios from 'axios'
 
 class SplashScreen extends React.Component {
   static navigationOptions = {
@@ -51,8 +52,8 @@ class SplashScreen extends React.Component {
 
           <TextInput
             style={styles.searchBox}
-            onChangeText={(text) => this.setState({text})}
-            value={this.state.text}
+            onChangeText={(text) => global.username = text}
+            value={global.username}
           />
 
           <LinearGradient colors={['#E57C7A', '#E5A27A']} style={styles.enterUser}>
@@ -79,12 +80,53 @@ class JoinSession extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Joining...',
   });
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '...' };
+
+    this.state = {
+      latitude: 1,
+      longitude: 1,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
   render() {
     const { params } = this.props.navigation.state;
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>Third test here!</Text>
+        <MapView
+          initialRegion={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0,
+            longitudeDelta: 0,
+          }}
+        style={styles.mapCreate}></MapView>
+
+          <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Latitude: {this.state.latitude}</Text>
+            <Text>Longitude: {this.state.longitude}</Text>
+            {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+          </View>
+
       </View>
     );
   }
@@ -126,20 +168,20 @@ class CreateSession extends React.Component {
     return (
       <View>
 
-        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Latitude: {this.state.latitude}</Text>
-          <Text>Longitude: {this.state.longitude}</Text>
-          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
-        </View>
-
         <MapView
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
         style={styles.mapCreate}></MapView>
+
+          <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Latitude: {this.state.latitude}</Text>
+            <Text>Longitude: {this.state.longitude}</Text>
+            {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+          </View>
 
       </View>
     );
@@ -150,22 +192,63 @@ class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Home',
   });
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.state2 = { text2: '' };
+  }
+
   render() {
     const { params } = this.props.navigation.state;
     const { navigate } = this.props.navigation;
     return (
       <View>
         <LinearGradient colors={['#E57C7A', '#E5A27A']} style={styles.joinButton}>
+
+          <TextInput
+            style={styles.joinBox}
+            onChangeText={(text) => this.setState({text1: text})}
+            value={this.state.text1}
+          />
           <Button
-            onPress={() => navigate('Join')}
+          onPress={() => {
+            console.log(this.state.text1)
+            fetch(`http://91adbe76.ngrok.io/room/${this.state.text1}/join/${global.username}`, {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+              }
+            }).then((res) => console.log(res))
+            .catch((err) => console.log(err))
+            navigate('Join')}
+          }
             title="Join a Friend's Party"
           >
           </Button>
         </LinearGradient>
 
         <LinearGradient colors={['#5C258D', '#4389A2']} style={styles.createButton}>
+
+          <TextInput
+            style={styles.createBox}
+            onChangeText={(text) => this.setState({text2: text})}
+            value={this.state.text2}
+          />
           <Button
-            onPress={() => navigate('Create')}
+            onPress={() => {
+              console.log(this.state.text2)
+              fetch(`http://91adbe76.ngrok.io/room/${this.state.text2}/create`, {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                }
+              }).then((res) => console.log(res))
+              .catch((err) => console.log(err))
+              navigate('Create')}
+            }
             title="Create a Party"
           >
           </Button>
@@ -197,14 +280,20 @@ const styles = StyleSheet.create({
   },
   joinButton: {
     marginTop: 0,
-    paddingTop: 105,
+    paddingTop: -100,
     height: 255,
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingBottom: 100,
     backgroundColor: "rgba(0,0,0,0)",
   },
   createButton: {
     marginTop: 0,
-    paddingTop: 105,
+    paddingTop: -100,
     height: 255,
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingBottom: 100,
     backgroundColor: "rgba(0,0,0,0)",
   },
   linearGradient: {
@@ -231,8 +320,26 @@ const styles = StyleSheet.create({
   },
   mapCreate: {
     width: 400,
-    height: 500,
-  }
+    height: 510,
+  },
+  createBox: {
+    borderRadius: 6,
+    marginTop: 90,
+    paddingLeft: 15,
+    height: 50,
+    backgroundColor: "#4389A2",
+    color: 'white',
+    fontFamily: "Avenir-Light",
+  },
+  joinBox: {
+    borderRadius: 6,
+    marginTop: 90,
+    paddingLeft: 15,
+    height: 50,
+    backgroundColor: "#4389A2",
+    color: 'white',
+    fontFamily: "Avenir-Light",
+  },
 });
 
 AppRegistry.registerComponent('mus_mark2', () => mus_mark2);
