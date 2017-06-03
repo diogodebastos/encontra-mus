e536b974c870a1cf789b3983dbd33da1c70f024b
simaonogueira101 <simao.nogueira66@gmail.com>
2017-06-03 10:28:28 +0100

    Adds maps to app client.

--- app_client/App.js
+++ app_client/App.js
@@ -8,9 +8,11 @@ import {
   StyleSheet,
   Image,
   TextInput,
+  WebView,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import LinearGradient from 'react-native-linear-gradient';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 class SplashScreen extends React.Component {
   static navigationOptions = {
@@ -19,7 +21,27 @@ class SplashScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { text: 'Insert Party Name...' };
+    this.state = { text: '...' };
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
   }
 
   render() {
@@ -33,21 +55,19 @@ class SplashScreen extends React.Component {
             value={this.state.text}
           />
 
-          <LinearGradient colors={['#E57C7A', '#E5A27A']} style={styles.joinButton}>
+          <LinearGradient colors={['#E57C7A', '#E5A27A']} style={styles.enterUser}>
             <Button
-              onPress={() => navigate('Join')}
-              title="Join a Friend's Session"
+              onPress={() => navigate('Home')}
+              title="Enter Username"
             >
             </Button>
           </LinearGradient>
 
-          <LinearGradient colors={['#E57C7A', '#E5A27A']} style={styles.createButton}>
-            <Button
-              onPress={() => navigate('Create')}
-              title="Create a Session"
-            >
-            </Button>
-          </LinearGradient>
+          <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Latitude: {this.state.latitude}</Text>
+            <Text>Longitude: {this.state.longitude}</Text>
+            {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+          </View>
 
         <Text style={styles.textSplash}>müs</Text>
       </LinearGradient>
@@ -61,6 +81,7 @@ class JoinSession extends React.Component {
   });
   render() {
     const { params } = this.props.navigation.state;
+    const { navigate } = this.props.navigation;
     return (
       <View>
         <Text>Third test here!</Text>
@@ -73,11 +94,83 @@ class CreateSession extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Creating...',
   });
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '...' };
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
   render() {
     const { params } = this.props.navigation.state;
+    const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>Fourth test here!</Text>
+
+        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Latitude: {this.state.latitude}</Text>
+          <Text>Longitude: {this.state.longitude}</Text>
+          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+        </View>
+
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        style={styles.mapCreate}></MapView>
+
+      </View>
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Home',
+  });
+  render() {
+    const { params } = this.props.navigation.state;
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <LinearGradient colors={['#E57C7A', '#E5A27A']} style={styles.joinButton}>
+          <Button
+            onPress={() => navigate('Join')}
+            title="Join a Friend's Party"
+          >
+          </Button>
+        </LinearGradient>
+
+        <LinearGradient colors={['#5C258D', '#4389A2']} style={styles.createButton}>
+          <Button
+            onPress={() => navigate('Create')}
+            title="Create a Party"
+          >
+          </Button>
+        </LinearGradient>
+
       </View>
     );
   }
@@ -88,6 +181,7 @@ const mus_mark2 = StackNavigator({
   Splash: { screen: SplashScreen },
   Join: { screen: JoinSession },
   Create: { screen: CreateSession },
+  Home: { screen: HomeScreen },
 });
 
 const styles = StyleSheet.create({
@@ -102,17 +196,15 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0,0,0,0)",
   },
   joinButton: {
-    marginTop: 12,
-    borderRadius: 6,
-    paddingTop: 5,
-    height: 50,
+    marginTop: 0,
+    paddingTop: 105,
+    height: 255,
     backgroundColor: "rgba(0,0,0,0)",
   },
   createButton: {
-    marginTop: 12,
-    borderRadius: 6,
-    paddingTop: 5,
-    height: 50,
+    marginTop: 0,
+    paddingTop: 105,
+    height: 255,
     backgroundColor: "rgba(0,0,0,0)",
   },
   linearGradient: {
@@ -128,6 +220,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#4389A2",
     color: 'white',
     fontFamily: "Avenir-Light",
+  },
+  enterUser: {
+    marginTop: 12,
+    borderRadius: 6,
+    paddingTop: 5,
+    height: 50,
+    backgroundColor: "rgba(0,0,0,0)",
+    marginBottom: 29,
+  },
+  mapCreate: {
+    width: 400,
+    height: 500,
   }
 });
 
